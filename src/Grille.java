6bde27f14543c35d3bb74657f13dc15b922d2457
cedricvignandel <cedric.vignandel@gmail.com>
2014-02-24 17:01:37 +0100

    déclaration d'atributs dans la classe grille

--- src/Grille.java
+++ src/Grille.java
@@ -5,7 +5,18 @@
  */
 public class Grille
 {
-	
+	/**
+	 * Dans la grille le 0 sera une case vide
+	 */
+	private final int CASE_VIDE=0;
+	/**
+	 * Dans la grille le 1 sera une croix
+	 */
+	private final int CROIX=1;
+	/**
+	 * Dans la grille le 2 sera un rond
+	 */
+	private final int ROND=2;
 	/**
 	 * ligne de la grille
 	 */
@@ -18,6 +29,14 @@ public class Grille
 	 * déclaration de la grille
 	 */
 	private int [][] grille ;
-	
+	/**
+	 * 
+	 */
+	public void position (int l,int c)
+	{
+		ligne=l;
+		colonne=c;
+		
+	}
 	
 }
